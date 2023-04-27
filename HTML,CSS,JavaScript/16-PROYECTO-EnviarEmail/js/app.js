dc942c0a5a94d3bc85035950af2c38e69cc6c518
Adan <>
2023-04-26 23:21:41 -0400

    fggh

---
+++ HTML,CSS,JavaScript/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener(DOmContentLoaded, function() {
+
+    //seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+
+});
