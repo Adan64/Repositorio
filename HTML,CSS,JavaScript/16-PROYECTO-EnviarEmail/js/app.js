e826164bfd5c5b7f1ec34487d9fff44d628ec33b
Adan <>
2023-05-04 22:48:42 -0400

    asdf

--- HTML,CSS,JavaScript/16-PROYECTO-EnviarEmail/js/app.js
+++ HTML,CSS,JavaScript/16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //asignar eventos
     inputEmail.addEventListener('blur',validar);
@@ -13,11 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
     function validar(e) {
         //trim elimina los espacios en blanco, importante poner en los formularios para validar
         if(e.target.value.trim() ===''){
-            
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
         }else{
 
         }
     }
 
+    function mostrarAlerta(mensaje, referencia) {
+        //generar alerta en HTML
+        const error = document.createElement('div');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        //inyectar el error al formulario
+        referencia.appendChild(error);
+        //otra forma es
+        /* formulario.innerHTML = error.innerHTML; */
+
+    }
 
 });
