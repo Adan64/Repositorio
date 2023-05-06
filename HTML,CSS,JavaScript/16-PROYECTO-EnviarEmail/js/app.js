76f94cc58fd18975fb561ef81f50d6248fe58fc6
Adan <>
2023-05-05 23:50:03 -0400

    asas

--- HTML,CSS,JavaScript/16-PROYECTO-EnviarEmail/js/app.js
+++ HTML,CSS,JavaScript/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const email= {
+        email: '',
+        asunto:'',
+        mensaje:'',
+    }
 
     //seleccionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
@@ -15,22 +20,56 @@ document.addEventListener('DOMContentLoaded', function() {
         //trim elimina los espacios en blanco, importante poner en los formularios para validar
         if(e.target.value.trim() ===''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        }else{
-
+            return;
         }
+
+        if(e.target.id=== 'email' &&  !validarEmail(e.target.value)){
+            mostrarAlerta('El campo no es valido', e.target.parentElement);
+            return;
+        }
+
+        limpiarAlerta( e.target.parentElement);
+
+        //asignar los valores
+        email[e.target.email]= e.target.value.trim().toLowerCase();
+        
+        //COMPROBAR EL OBJETO DE EMAIL
+        comprobarEmail(email);
     }
 
     function mostrarAlerta(mensaje, referencia) {
+        limpiarAlerta(referencia);
+        
+        
         //generar alerta en HTML
         const error = document.createElement('div');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
-
+        
         //inyectar el error al formulario
         referencia.appendChild(error);
         //otra forma es
         /* formulario.innerHTML = error.innerHTML; */
+        
+    }
+    
+    function limpiarAlerta(referencia){
+        //comprueab si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
 
     }
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+        return resultado;
+    }
 
+    function comprobarEmail(){
+        console.log(email);
+    }
 });
+
+//ITAIPUUU
